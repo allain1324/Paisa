2316eed4a8a6ee3e1feb83adbc329967cc02720e
Rahul <82636553+rahulkhatri137@users.noreply.github.com>
2024-04-11 20:24:04 +0530

    Fix `GitHub` link

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,6 +1,5 @@
-const gitHubUrl = 'https://github.com/h4h13/paisa';
-const playStoreUrl =
-    'https://play.google.com/store/apps/details?id=dev.hemanths.paisa';
+const gitHubUrl = 'https://github.com/RetroMusicPlayer/Paisa';
+const playStoreUrl = 'https://play.google.com/store/apps/details?id=dev.hemanths.paisa';
 const telegramGroupUrl = 'https://t.me/app_paisa';
 const termsAndConditionsUrl = 'https://hemanths.dev/privacy';
 
